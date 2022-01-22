55aca8803763dc42dc26f8354611e8157c745987
RJLimBU <kevinlim@bu.edu>
2022-01-22 12:38:26 -0500

    basic main console added

---
+++ mainloop.py
@@ -0,0 +1,10 @@
+#main console
+
+
+if __name__ == '__main__':
+	print("\n----------------------------------------\n")
+	print("Hello! This is a word analyzer.\n")
+	filepath = input('Please enter path of your file: ')
+
+	print("processing file: %s" % filepath)
+
