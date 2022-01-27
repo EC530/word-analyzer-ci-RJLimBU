2659ee39958c18282e60053c91819c1c7ca3f5cf
RJLimBU <kevinlim@bu.edu>
2022-01-26 23:52:11 -0500

    add basic functionality and test

--- mainloop.py
+++ mainloop.py
@@ -1,12 +1,62 @@
 #main console
+import matplotlib.pyplot as plt
+#import numpy as np
 
-def getfilepath():
-	filepath = input('Please enter path of your file: ')
-	return filepath
+class wordfreq:
+	def __init__(self):
+		#uniqword = []
+		#countword = []
+		a = 0
 
-if __name__ == '__main__':
+	def getfilepath():
+		filepath = input('Please enter path of your file: ')
+		return filepath
+
+	def openfile(fp):
+		f = open(fp,'r')
+		txt = f.read()
+		txt = txt.split()
+		f.close()
+		return txt
+
+	def getwords(txt):
+		uniquewords = []
+		for word in txt:
+			if word not in uniquewords:
+				uniquewords.append(word)
+
+		return uniquewords
+
+	def countwords(txt, uniquewords):
+		l = len(uniquewords)
+		count = []
+		for i in range(l):
+			count.append(txt.count(uniquewords[i]))
+
+		return count
+
+	def plotfreq(uniqword, countword):
+		plt.bar(uniqword, countword, align = 'center')
+		plt.xlabel('Words')
+		plt.ylabel('Frequency')
+		plt.show()
+
+def main():
 	print("\n-------------------------------------------\n")
 	print("Hello! This is a word analyzer.\n")
-	fp = getfilepath()
+	fp = wordfreq.getfilepath()
+	print("\n-----------processing file: %s-----------\n" % fp)
 
-	print("processing file: %s" % fp)
+	txt = wordfreq.openfile(fp)
+
+	uniqword = wordfreq.getwords(txt)
+
+	countword = wordfreq.countwords(txt, uniqword)
+
+	print(len(uniqword))
+	print(len(countword))
+	wordfreq.plotfreq(uniqword,countword)
+	#print("Please enter a valid path!!!")
+
+if __name__ == '__main__':
+	main()
