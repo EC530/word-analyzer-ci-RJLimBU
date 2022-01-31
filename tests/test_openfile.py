a835a79f058e8e6ea5d4c321b578dce5c85b545a
RJLimBU <kevinlim@bu.edu>
2022-01-30 23:02:52 -0500

    openfiletest update

--- tests/test_openfile.py
+++ tests/test_openfile.py
@@ -2,7 +2,13 @@ import sys
 #sys.path.append('../word-analyzer-ci-RJLimBU')
 from mainloop import wordfreq
 
-def test_openfile():
+def test_openexample1():
 	name = wordfreq.openfile("example.txt")
+	txt = "hello, this is an example txt file to read\nI wandered lonely as a cloud\nThat floats on high over vales and hills\nWhen all at once I saw a crowd\nA host of golden worker ants.\n"
 	for i in range(len(name)):
-		assert isinstance(name[i],str)
+		assert name[i] == txt[i]
+
+def test_openexample2():
+	name = wordfreq.openfile("example2.txt")
+	assert name[0] == 'B'
+	assert name[-1] == '!'
